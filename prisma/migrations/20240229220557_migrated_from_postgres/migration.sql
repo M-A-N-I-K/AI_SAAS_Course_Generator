39f9890e56360348186e7c04d75a07ed3655bda7
Manik Dingra <95385759+M-A-N-I-K@users.noreply.github.com>
2024-03-28 17:34:13 +0530

    course type changed

---
+++ prisma/migrations/20240229220557_migrated_from_postgres/migration.sql
@@ -0,0 +1,87 @@
+-- CreateTable
+CREATE TABLE `Account` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `type` VARCHAR(191) NOT NULL,
+    `provider` VARCHAR(191) NOT NULL,
+    `providerAccountId` VARCHAR(191) NOT NULL,
+    `refresh_token` TEXT NULL,
+    `access_token` TEXT NULL,
+    `expires_at` INTEGER NULL,
+    `token_type` VARCHAR(191) NULL,
+    `scope` VARCHAR(191) NULL,
+    `id_token` TEXT NULL,
+    `session_state` VARCHAR(191) NULL,
+
+    INDEX `userId`(`userId`),
+    UNIQUE INDEX `Account_provider_providerAccountId_key`(`provider`, `providerAccountId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Session` (
+    `id` VARCHAR(191) NOT NULL,
+    `sessionToken` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `expires` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Session_sessionToken_key`(`sessionToken`),
+    INDEX `userId`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NULL,
+    `emailVerified` DATETIME(3) NULL,
+    `image` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Course` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `image` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Unit` (
+    `id` VARCHAR(191) NOT NULL,
+    `courseId` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+
+    INDEX `courseId`(`courseId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Chapter` (
+    `id` VARCHAR(191) NOT NULL,
+    `unitId` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `youtubeSearchQuery` VARCHAR(191) NOT NULL,
+    `videoId` VARCHAR(191) NULL,
+    `summary` VARCHAR(3000) NULL,
+
+    INDEX `unitId`(`unitId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Question` (
+    `id` VARCHAR(191) NOT NULL,
+    `chapterId` VARCHAR(191) NOT NULL,
+    `question` VARCHAR(3000) NOT NULL,
+    `answer` VARCHAR(3000) NOT NULL,
+    `options` VARCHAR(3000) NOT NULL,
+
+    INDEX `chapterId`(`chapterId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -6,33 +6,33 @@ import React from "react";
 type Props = {};
 
 const GalleryPage = async (props: Props) => {
-  const courses = await prisma.course.findMany({
-    include: {
-      units: {
-        include: { chapters: true },
-      },
-    },
-  });
+	const courses = await prisma.course.findMany({
+		include: {
+			units: {
+				include: { chapters: true },
+			},
+		},
+	});
 
-  return (
-    <div>
-      <div className="flex p-4 mt-5 border-none bg-secondary">
-        <BookOpenIcon className="w-12 h-12 mr-3 text-blue-400" />
-        <div>
-          Your AI-generated study resources will be conveniently accessible here
-          for future reference, ensuring a seamless and effective learning
-          experience.
-        </div>
-      </div>
-      <div className="py-8 mx-auto max-w-7xl">
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 place-items-center">
-          {courses.map((course) => (
-            <GalleryCourseCard course={course} key={course.id} />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="flex p-4 mt-5 border-none bg-secondary">
+				<BookOpenIcon className="w-12 h-12 mr-3 text-blue-400" />
+				<div>
+					Your AI-generated study resources will be conveniently accessible
+					here for future reference, ensuring a seamless and effective
+					learning experience.
+				</div>
+			</div>
+			<div className="py-8 mx-auto max-w-7xl">
+				<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 place-items-center">
+					{courses.map((course: any) => (
+						<GalleryCourseCard course={course} key={course.id} />
+					))}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default GalleryPage;
