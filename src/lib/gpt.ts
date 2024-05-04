4f065d933d9ab4bcb4e6ab9a770ca18d98213485
M-A-N-I-K <manikdhingra0582@gmail.com>
2024-05-04 22:38:30 +0530

    ui components modified

--- src/lib/gpt.ts
+++ src/lib/gpt.ts
@@ -14,7 +14,7 @@ export async function strict_output(
 	output_format: OutputFormat,
 	default_category: string = "",
 	output_value_only: boolean = false,
-	model: string = "gpt-3.5-turbo",
+	model: string = "gpt-4",
 	temperature: number = 1,
 	num_tries: number = 3,
 	verbose: boolean = false
